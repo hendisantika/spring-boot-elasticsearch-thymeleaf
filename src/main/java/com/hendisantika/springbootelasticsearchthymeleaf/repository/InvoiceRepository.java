1d97561bc917a7efd673c542ea3c9c68118feaa8
hendisantika <hendisantika@yahoo.co.id>
2023-10-26 12:19:16 +0700

    #7 - Add getInvoiceById in Invoice Repository ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springbootelasticsearchthymeleaf/repository/InvoiceRepository.java
+++ src/main/java/com/hendisantika/springbootelasticsearchthymeleaf/repository/InvoiceRepository.java
@@ -1,9 +1,11 @@
 package com.hendisantika.springbootelasticsearchthymeleaf.repository;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.GetResponse;
 import co.elastic.clients.elasticsearch.core.IndexResponse;
 import com.hendisantika.springbootelasticsearchthymeleaf.entity.Invoice;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
@@ -18,6 +20,7 @@ import java.io.IOException;
  * Time: 11:35
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class InvoiceRepository {
@@ -38,6 +41,23 @@ public class InvoiceRepository {
             return "Invoice document has been updated successfully.";
         }
         return "Error while performing the operation.";
+    }
+
+    public Invoice getInvoiceById(String invoiceId) throws IOException {
+        Invoice invoice = null;
+        GetResponse<Invoice> response = elasticsearchClient.get(
+                g -> g.index(indexName)
+                        .id(invoiceId),
+                Invoice.class
+        );
+
+        if (response.found()) {
+            invoice = response.source();
+            log.info("Invoice name is: " + invoice.getName());
+        } else {
+            log.info("Invoice not found");
+        }
+        return invoice;
     }
 
 }
