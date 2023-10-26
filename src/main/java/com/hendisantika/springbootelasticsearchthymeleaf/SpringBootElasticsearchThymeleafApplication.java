e944de01bbd3dad7fffe89161d720c28f2eee587
hendisantika <hendisantika@yahoo.co.id>
2023-10-26 11:30:37 +0700

    #1 - Initial commit ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springbootelasticsearchthymeleaf/SpringBootElasticsearchThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootelasticsearchthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElasticsearchThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootElasticsearchThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootelasticsearchthymeleaf/SpringBootElasticsearchThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootelasticsearchthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElasticsearchThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
