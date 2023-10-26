df6e20aea2b5ae5e26f8040d0cc7d34869b5d476
hendisantika <hendisantika@yahoo.co.id>
2023-10-26 11:34:29 +0700

    #3 - Add InvoiceConfig ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springbootelasticsearchthymeleaf/config/InvoiceConfig.java
@@ -0,0 +1,41 @@
+package com.hendisantika.springbootelasticsearchthymeleaf.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-elasticsearch-thymeleaf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/26/23
+ * Time: 11:33
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class InvoiceConfig {
+
+    @Bean
+    public RestClient getRestClient() {
+        RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200)).build();
+        return restClient;
+    }
+
+    @Bean
+    public ElasticsearchTransport getElasticsearchTransport() {
+        return new RestClientTransport(getRestClient(), new JacksonJsonpMapper());
+    }
+
+    @Bean
+    public ElasticsearchClient getElasticsearchClient() {
+        ElasticsearchClient client = new ElasticsearchClient(getElasticsearchTransport());
+        return client;
+    }
+}
