05f16e55acffca10d46908cee7c0d3eeb0db4ac2
hendisantika <hendisantika@yahoo.co.id>
2023-10-26 12:24:07 +0700

    #15 - Add InvoiceController ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springbootelasticsearchthymeleaf/controller/InvoiceController.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootelasticsearchthymeleaf.controller;
+
+import com.hendisantika.springbootelasticsearchthymeleaf.repository.InvoiceRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-elasticsearch-thymeleaf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/26/23
+ * Time: 12:23
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class InvoiceController {
+
+    private final InvoiceRepository invoiceRepository;
+}
